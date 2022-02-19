f62c56212fe9f802b1b5e5fd4531a46503ffb8e3
Fariz <49908901+22Fariz22@users.noreply.github.com>
2022-02-19 23:41:14 +0300

    delete and update only own posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -34,41 +34,57 @@ async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db),
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-
     return new_post
 
-
-# @router.get('/latest')
-# def latest_post():
-#     last: str = bd[-1]
-#     print(last)
-#     return last
 
 @router.get('/{id}', response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db),
              current_user: int = Depends(oauth2.get_current_user)):
-
     post = db.query(models.Post).filter(models.Post.id==id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id {id} was not found')
     return post
 
-@router.delete('/posts/{id}',status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 
-     post = db.query(models.Post).filter(models.Post.id == id)
-     if post.first() == None:
-         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post id={id} was not found')
-     post.delete(synchronize_session=False)
+@router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db),
+                current_user: int = Depends(oauth2.get_current_user)):
+
+     post_query = db.query(models.Post).filter(models.Post.id == id)
+     post = post_query.first()
+
+     # if own_id[0] == current_user.id:
+     #     post.delete(synchronize_session=False)
+
+     if post == None:
+         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                             detail=f'post id={id} was not found')
+
+     if post.owner_id != current_user.id:
+         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                             detail='Not authorized to perform requested action')
+
+     post_query.delete(synchronize_session=False)
      db.commit()
+     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@router.put('/{id}',response_model=schemas.Post)
-def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    query_post = db.query(models.Post).filter(models.Post.id == id)
-    first_post = query_post.first()
-    if first_post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='this post was not found')
-    query_post.update(post.dict(), synchronize_session=False)
+
+@router.put('/{id}', response_model=schemas.Post)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db),
+                current_user: int = Depends(oauth2.get_current_user)):
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='this post was not found')
+
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail='Not authorized to perform requested action')
+
+    post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
-    return 'succsess'
+    return post_query.first()
 
